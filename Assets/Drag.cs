90c6a6f2606469aa49e23b71fd290c285bbf89aa
aborodulin84 <aborodulin84@gmail.com>
2017-05-14 12:12:16 +0400

    added

---
+++ Assets/Drag.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+    #region переменные
+    float posX;
+    float posY;
+    float Impulse;
+    int i = 0;
+    Vector3 Start;
+    Vector3 dist;
+    Vector3[] Points = null;
+    class Line
+    {
+        public float a;
+            public float b;
+       public Line(Vector3 i,Vector3 j)
+        {
+            a = (i.y - j.y) / (i.x - j.x);
+            b = i.y - (i.y - j.y) / (i.x - j.x) * i.x;
+        }
+    }
+    LineRenderer lineRenderer;
+    #endregion
+
+    #region функции
+    void PosInit()  //подбор координат шарика
+    {
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+    }
+
+    void CarryBall() //перенос шарика
+    {
+        Vector3 curPos =
+                  new Vector3(Input.mousePosition.x - posX,
+                  Input.mousePosition.y - posY, dist.z);
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+        transform.position = worldPos;
+    }
+
+    float meterImpulse(Vector3 a, Vector3 b)
+    {
+        return Mathf.Sqrt(Mathf.Pow(a.x - b.x, 2) + Mathf.Pow(a.y - b.y, 2));
+    }
+
+    void RenderLine(Vector3 i, Vector3 j )
+    {
+        Line tr = new Line(i, j);
+        lineRenderer.SetPosition(0, transform.position);
+        if (Start.x == transform.position.x) lineRenderer.SetPosition(1, new Vector3(transform.position.x, 6, 1));
+        else
+        {
+            lineRenderer.SetPosition(1, new Vector3((transform.position.x > 0 ? -8.5f : 8.5f), tr.a*(transform.position.x > 0 ? -8.5f : 8.5f) +tr.b, 0));
+            lineRenderer.SetPosition(2, new Vector3((transform.position.x < 0 ? -8.5f : 8.5f), tr.a*(lineRenderer.GetPosition(1).x), 0));
+        }
+    }
+
+    #endregion
+
+   private void OnMouseDown()
+    {
+        Start = transform.position;
+        lineRenderer = GetComponent<LineRenderer>();
+        PosInit();
+        lineRenderer.enabled = true;
+    }
+
+   private void OnMouseDrag()
+    {
+        CarryBall();
+        RenderLine(Start, transform.position);
+        Impulse = meterImpulse(Start, transform.position);
+    }
+
+    private void OnMouseUp()
+    {
+        Points = new Vector3[3];
+        lineRenderer.GetPositions(Points);
+        lineRenderer.enabled = false;
+        foreach(Vector3 p in Points)
+        {
+            Debug.Log(p);
+        }
+    }
+
+    private void Update()
+    {
+        if (i == 2) { Points = null; i = 0; }
+        if (Points != null)
+        {
+            Vector3 dir = (Points[i + 1] - transform.position).normalized * Impulse * Time.deltaTime * 10;
+            transform.Translate(dir); // Vector3.Lerp(transform.position, Points[i+1], Vector3.Distance(Start,Dash)*Time.deltaTime);
+            if (transform.position.x > 8 || transform.position.x < -8) i++;
+
+        }
+    }
+
+}
---
+++ Assets/InstBlock.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Xml.Linq;
+
+public class InstBlock : MonoBehaviour {
+    #region переменные
+    //игровой объект
+    public GameObject Circle;
+    public GameObject Cue;
+    //структура цвет
+    struct RGBcolor
+    {
+       public float R;
+       public float G;
+       public float B;
+       public float A;
+    }
+    #endregion
+
+    #region функции
+    //функция парсинга цвета
+    public Color parseColor(string s)
+    {
+        RGBcolor c;
+        c.R = float.Parse(s.Substring(0, 5));
+        c.G = float.Parse(s.Substring(7, 5));
+        c.B = float.Parse(s.Substring(14, 5));
+        c.A = 1f;
+        return new Color(c.R, c.G, c.B, c.A);
+    }
+    // парсинг координат
+    public Vector3 parseVector(string v)
+    {
+        string[] coord = v.Split(',');
+        string q = coord[0] + coord[1];
+        coord = q.Split();
+        return new Vector3(float.Parse(coord[0]), float.Parse(coord[1]),0);
+    }
+    // построение игрового поля
+    public void BildPole()
+    {
+        var pole = XDocument.Parse(System.IO.File.ReadAllText("block.xml")).Descendants("Ball").ToList<XElement>();
+
+        foreach (var e in pole)
+        {
+            Circle.GetComponent<SpriteRenderer>().color = parseColor((e.Attribute("color").Value).Substring(5, 26));
+            Instantiate(Circle, parseVector((e.Attribute("transform").Value).Substring(1, 9)), Quaternion.identity);
+        }
+    }
+    // позиционирование шара-биты
+    public void CueBall()
+    {
+        var pole = XDocument.Parse(System.IO.File.ReadAllText("block.xml")).Descendants("Ball").ToList<XElement>();
+        Cue.GetComponent<SpriteRenderer>().color = parseColor((pole[Random.Range(0, pole.Count)].Attribute("color").Value).Substring(5, 26));
+        Instantiate(Cue,new Vector3(0,-3,0),Quaternion.identity);
+    }
+    #endregion
+
+    void Start() {
+
+        BildPole();
+        CueBall();
+    }
+
+
+
+}
+	
+	
+
